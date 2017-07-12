3fefd124742ee6aedc27693b961f4a3fb3773126
wenyuhang <837572162@qq.com>
2017-07-12 16:04:30 +0800

    温宇航第4次修改

--- app/src/main/java/com/example/apps/HomeActivity.java
+++ app/src/main/java/com/example/apps/HomeActivity.java
@@ -18,7 +18,7 @@ import com.example.apps.homes.homerollvideo.RollVideoMain;
 
 import butterknife.Bind;
 import butterknife.OnClick;
-//3-3
+//3-4
 public class HomeActivity extends BaseActivity {
 
     @Bind(R.id.home_page)
